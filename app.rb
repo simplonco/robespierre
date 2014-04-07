261b7fd67f90769fccaf887c75d18c088d7af080
Ben2001 <ben@ordi-cretin.(none)>
2014-04-07 17:27:12 +0200

    le player_audio fonctionne

--- app.rb
+++ app.rb
@@ -12,16 +12,25 @@ client = Twitter::REST::Client.new do |config|
 	config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 
+class Track < ActiveRecord::Base
+end
+
 enable :sessions #permet de stocker une variable dans une session et de pouvoir l'utiliser partout dans l'app
+
+helpers do #regarder comment créer un helper sur sinatra
+	def foo
+		"foo"
+	end
+end
 
 get '/' do
 	@user_mentions  = client.mentions_timeline
 	erb :index
+	# binding.pry
 end
 
 post '/' do #avec tts
 	"#{params[:input]}".to_file "fr", "public/#{params[:input]}.mp3"
-	
 	redirect to ('/')
 	#{}`cvlc --play-and-exit "input.mp3" && rm input.mp3`
 end
@@ -35,7 +44,7 @@ post '/2' do
 	redirect to ('/2')
 end
 
-post '/search' do #pour chercher un mot o ou hashtag sur twitter 
+post '/search' do #pour chercher un mot o ou hashtag sur twitter
 	@search = client.search("#{params[:input]}")
 	erb :search_page
 end
@@ -49,12 +58,21 @@ post '/5' do #envoie vers le player
 	redirect to ('/')
 end
 
-post '/6' do #envoie les morceaux de la playlist au player
-	session[:track] = params[:track].gsub(/"\[\]/, '').gsub(',', '|')
-	redirect to ('/2')
-end
+# post '/6' do #envoie les morceaux de la playlist au player
+# 	session[:track] = params[:track].gsub(/"\[\]/, '').gsub(',', '|')
+# 	redirect to ('/2')
+# end
 
 post '/arduino' do
 	arduino_controller = ArduinoControl.new(" blabla")
 	arduino_controller.blink_ten_times(1.0)
+end
+
+post '/sort_url' do
+	params[:tracks].each_with_index do |track_name, index|
+		Track.update_all({position: index+1}, {title: track_name})
+	end
+	Track.order("position").map do |track|
+		{title: track.title, mp3: track.lien}
+	end.to_json
 end
